1fa7fa011e143b319e5b32e2d65a9cf286413325
misoso519 <hamu.86.m@gmail.com>
2025-02-25 05:44:31 +0900

    Fix UploadToS3 class autoloading

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Dotenv.load
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -18,7 +19,7 @@ module Myapp
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
+    config.autoload_paths += %W(#{config.root}/lib)
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- lib/upload_to_s3.rb
+++ lib/upload_to_s3.rb
@@ -1,28 +1,33 @@
 require 'aws-sdk-s3'
 
-s3 = Aws::S3::Resource.new(region: 'ap-northeast-1')
+class UploadToS3
+  def initialize(region: 'ap-southeast-1', bucket_name: 'myapp3-bucket2')
+    @s3 = Aws::S3::Resource.new(region: region)
+    @bucket = @s3.bucket(bucket_name)
+  end
 
-# バケット名を指定（事前に作成したS3バケット名を設定）
-bucket = s3.bucket('myapp3-bucket2')  # バケット名を設定
+  def upload_directory(directories)
+    directories.each do |dir|
+      Dir.glob("public/uploads/#{dir}/**/*").each do |file_path|
+        if File.file?(file_path)
+          upload_file(file_path, dir)
+        end
+      end
+    end
+  end
 
-# アップロードするディレクトリを指定
-directories = ['answer', 'question', 'tmp']
+  private
 
-directories.each do |dir|
-  Dir.glob("public/uploads/#{dir}/**/*").each do |file_path|
-    if File.file?(file_path)
-      file = File.open(file_path, 'rb')
-      file_name = File.basename(file_path)
-      s3_object = bucket.object("#{dir}/#{file_name}")
+  def upload_file(file_path, dir)
+    file_name = File.basename(file_path)
+    s3_object = @bucket.object("#{dir}/#{file_name}")
 
-      # ファイルをS3にアップロード
+    # ファイルを開き、S3にアップロード
+    File.open(file_path, 'rb') do |file|
       s3_object.put(body: file)
 
       # 成功メッセージ
       puts "Uploaded #{file_name} from #{dir} to S3!"
-
-      # ファイルを閉じる
-      file.close
     end
   end
 end
