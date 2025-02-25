00c6d2030f11a46a16968c267102fd37e067ae38
misoso519 <hamu.86.m@gmail.com>
2025-02-25 18:58:23 +0900

    region明示的に

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
     provider:              'AWS',
-    region:                ENV['AWS_REGION'],
+    region:                'ap-northeast-1',
     aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
   }
