a1ac18fa6d01591a3023a7004937684a90bde64c
misoso519 <hamu.86.m@gmail.com>
2025-02-25 10:06:00 +0900

    Fix importmap and pin dependencies

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,5 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "https://cdn.skypack.dev/@hotwired/turbo-rails"
-import "./controllers";
+pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@8.0.12/app/javascript/turbo/index.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@8.0.12/dist/turbo.es2017-esm.js"
+pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@7.2.201/src/index.js"
