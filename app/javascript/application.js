5bd37d5f552c3568b34b4a86ad3a865f717d2e4b
misoso519 <hamu.86.m@gmail.com>
2025-02-25 16:19:23 +0900

    Remove duplicate application.js

--- app/javascript/application.js
+++
@@ -1,9 +0,0 @@
-import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-
-const application = Application.start()
-
-const context = require.context("controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
-
-export { application }
