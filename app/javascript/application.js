0690d738d69592db0829052423b02f91ceb80e18
misoso519 <hamu.86.m@gmail.com>
2025-02-25 10:17:14 +0900

    importmap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,2 @@
 import "@hotwired/turbo-rails"
-import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-
-const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
-
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus = application
-
-export { application }
+import "controllers"
