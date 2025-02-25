ad4f67603f9a3511d150dc44779e800366edbe05
misoso519 <hamu.86.m@gmail.com>
2025-02-25 09:49:14 +0900

    application.js

---
+++ app/javascript/application.js
@@ -0,0 +1,9 @@
+import "@hotwired/turbo-rails"
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-loading"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+import "./controllers"
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,5 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "https://cdn.skypack.dev/@hotwired/turbo-rails"
+import "./controllers";
+pin_all_from "app/javascript/controllers", under: "controllers"
