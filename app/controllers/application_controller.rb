0c233770892ebd9e49633c6e4b3ec92f1e23a0e9
misoso519 <hamu.86.m@gmail.com>
2025-02-27 00:34:13 +0900

    user edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :profile_image])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :bio, :profile_image])
   end
 end
