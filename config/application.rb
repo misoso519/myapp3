19db7b0cf1dea8ff79cb0132a65046ec277dffeb
misoso519 <hamu.86.m@gmail.com>
2025-05-04 22:10:25 +0900

    redirect2

--- config/application.rb
+++ config/application.rb
@@ -24,14 +24,12 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    # Move importmap configuration inside the Application class
+    config.middleware.insert_before 0, Rack::Redirect do
+      source 'http://kidsteachers.net'
+      destination 'http://www.kidsteachers.net'
+    end  
     config.importmap.draw do
       pin "@hotwired/turbo-rails"
     end  
-  end
-  config.middleware.insert_before 0, Rack::Redirect do
-    source 'http://kidsteachers.net'
-    destination 'http://www.kidsteachers.net'
   end
 end
